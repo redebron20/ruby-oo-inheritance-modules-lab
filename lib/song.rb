e979d2b5157b4cf8ce99bda1b56068a4407bc6ca
redebron20 <redebron20@gmail.com>
2020-11-20 21:15:58 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,8 @@
 require 'pry'
 
 class Song
+  extend Memorable
+
   attr_accessor :name
   attr_reader :artist
 
@@ -16,14 +18,6 @@ class Song
 
   def self.all
     @@songs
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def artist=(artist)
